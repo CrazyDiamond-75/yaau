0d5e028720804e6f7dea4da97ce34b570cc01f38
Henri Heyden <44402515+CrazyDiamond-75@users.noreply.github.com>
2025-10-09 16:38:37 +0200

    Added initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 style="font-variant:small-caps"> yaau - Yet Another Aur Updater </h1>
+`./yaau.sh --version`
+
+```
+yauu --- Yet Another AUR Updater
+Current version: v0.1
+Copyright Henri Heyden, licenced under MIT.
+```
+
+`./yaau.sh --help`
+
+```
+Usage: ./yaau.sh PATH
+Reports if the repositories in the directory are outdated.
+Updates all repositories and installs their packages.
+Optional arguments:
+    -h    --help      print this message.
+    -v    --version   display yaau version.
+```
---
+++ yaau.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+# Check the number of arguments, or if one is "--help", or "-h".
+if [ "$#" -ne 1 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    echo "Usage: ./yaau.sh PATH"
+    echo "Reports if the repositories in the directory are outdated."
+    echo "Updates all repositories and installs their packages."
+    echo "Optional arguments:"
+    echo "    -h    --help      print this message."
+    echo "    -v    --version   display yaau version."
+    exit
+fi
+
+# Check if version should be displayed.
+if [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
+    echo "yauu --- Yet Another AUR Updater"
+    echo "Current version: v0.1"
+    echo "Copyright Henri Heyden, licenced under MIT."
+    exit
+fi
+
+# Check if $1 is really a fitting path.
+if [ ! -d $1 ]; then
+    echo "$1 is not a path to a directory."
+    # Recursively call yauu.sh --help
+    sh -c "$0 --help"
+    exit
+fi
+
+DIR=$1
+echo "Checking all repos in $DIR for updates."
+UPDATE_COUNT=0
+REPO_LIST=""
+
+# Go through all indexes in the directory, and do the magic...
+for SUB_DIR in $DIR/*; do
+    # Check if SUB_DIR is really a directory of a repository.
+    if [ ! -d $SUB_DIR/.git ]; then
+        continue
+    fi
+
+    # Fetch the repos to refresh origin/master.
+    # Redirect stdout to /dev/null.
+    git -C $SUB_DIR fetch 1> /dev/null
+
+    # Check if master and origin/master differ.
+    OUTPUT=$(git -C $SUB_DIR status -sb)
+    case $OUTPUT in
+        *"behind"*)
+            echo Repository \"$SUB_DIR\" can be updated.
+            UPDATE_COUNT=$(($UPDATE_COUNT + 1))
+            REPO_LIST="$REPO_LIST,$SUB_DIR"
+    esac
+done
+
+if [ $UPDATE_COUNT -eq 0 ]; then
+    echo "There are currently no possible updates."
+    exit
+fi
+
+echo "There are currently $UPDATE_COUNT possible updates."
+echo "Upgrade? [Y/n]"
+read PROMPT
+if [ ! "$PROMPT" = "Y" ] && [ ! "$PROMPT" = "" ]; then
+    exit
+fi
+
+echo "Updating..."
+
+# Remove the first element of the list, as it is empty.
+REPO_LIST=$(echo "$REPO_LIST" | cut -c 2-)
+
+# Setup for iterating though the list
+OLD_IFS=$IFS
+IFS=","
+set -- $REPO_LIST # Sets $0, $1, etc to the relevant elements.
+IFS=$OLD_IFS
+
+# Go through all 
+for SUB_DIR; do
+    git -C $SUB_DIR pull 1> /dev/null
+    makepkg -sirc -D $SUB_DIR
+    echo "Updated package from $SUB_DIR".
+done
