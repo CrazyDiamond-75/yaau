20038985ce2a1c784335cc60174faf23d1664261
Henri Heyden <henri@heyden5.de>
2025-10-18 15:26:20 +0200

    Added option to force reinstall packages if needed

--- yaau.sh
+++ yaau.sh
@@ -1,34 +1,62 @@
 #!/usr/bin/env bash
 
 # Check the number of arguments, or if one is "--help", or "-h".
-if [ "$#" -ne 1 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-    echo "Usage: ./yaau.sh PATH"
-    echo "Reports if the repositories in the directory are outdated."
-    echo "Updates all repositories and installs their packages."
-    echo "Optional arguments:"
-    echo "    -h    --help      print this message."
-    echo "    -v    --version   display yaau version."
+if [ "$#" -lt 1 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
+    cat << EOF
+Usage: ./yaau.sh PATH
+Reports if the repositories in the directory are outdated.
+Updates all repositories and installs their packages.
+Optional arguments:
+    -h    --help      print this message.
+    -v    --version   display yaau version.
+    -f    --force     force installs all packages.
+EOF
     exit
 fi
 
 # Check if version should be displayed.
 if [ "$1" = "--version" ] || [ "$1" = "-v" ]; then
-    printf "yauu --- Yet Another AUR Updater"
-    printf "Current version: v0.1"
-    printf "Copyright Henri Heyden, licenced under MIT."
+    cat << EOF
+yauu --- Yet Another AUR Updater
+Current version: v0.1
+Copyright Henri Heyden, licenced under MIT.
+EOF
     exit
 fi
 
 # Check if $1 is really a fitting path.
-if [ ! -d $1 ]; then
-    printf "$1 is not a path to a directory."
+DIR="${!#}"
+if [ ! -d $DIR ]; then
+    printf "$DIR is not a path to a directory.\n"
     # Recursively call yauu.sh --help
     sh -c "$0 --help"
     exit
 fi
 
-DIR=$1
-printf "Checking all repos in $DIR for updates."
+# FORCE INSTALL PATH
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
+    printf "Warning: Installing all outdated packages.\n"
+    printf "Confirm? [Y/n]\n"
+    read PROMPT
+    if [ ! "$PROMPT" = "Y" ] && [ ! "$PROMPT" = "" ]; then
+        exit
+    fi
+
+    for SUB_DIR in $DIR/*; do
+        # Check if SUB_DIR is really a directory of a repository.
+        if [ ! -d $SUB_DIR/.git ]; then
+            continue
+        fi
+        
+        makepkg --needed -sirc -D $SUB_DIR
+        printf "Installed package from $SUB_DIR.\n"
+    done
+    
+    exit
+fi
+
+# DEFAULT BEHAVIOUR
+printf "Checking all repos in $DIR for updates.\n"
 UPDATE_COUNT=0
 REPO_LIST=""
 
@@ -47,25 +75,25 @@ for SUB_DIR in $DIR/*; do
     OUTPUT=$(git -C $SUB_DIR status -sb)
     case $OUTPUT in
         *"behind"*)
-            printf "Repository \"$SUB_DIR\" can be updated."
+            printf "Repository \"$SUB_DIR\" can be updated.\n"
             UPDATE_COUNT=$(($UPDATE_COUNT + 1))
             REPO_LIST="$REPO_LIST,$SUB_DIR"
     esac
 done
 
 if [ $UPDATE_COUNT -eq 0 ]; then
-    printf "There are currently no possible updates."
+    printf "There are currently no possible updates.\n"
     exit
 fi
 
-printf "There are currently $UPDATE_COUNT possible updates."
-printf "Upgrade? [Y/n]"
+printf "There are currently $UPDATE_COUNT possible updates.\n"
+printf "Upgrade? [Y/n]\n"
 read PROMPT
 if [ ! "$PROMPT" = "Y" ] && [ ! "$PROMPT" = "" ]; then
     exit
 fi
 
-printf "Updating..."
+printf "Updating...\n"
 
 # Remove the first element of the list, as it is empty.
 REPO_LIST=$(printf "$REPO_LIST" | cut -c 2-)
@@ -80,5 +108,5 @@ IFS=$OLD_IFS
 for SUB_DIR; do
     git -C $SUB_DIR pull 1> /dev/null
     makepkg -sirc -D $SUB_DIR
-    printf "Updated package from $SUB_DIR".
+    printf "Updated package from $SUB_DIR\n".
 done
