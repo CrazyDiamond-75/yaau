f3b64a2a8d0a97feb367e22217d4c29004f76ca8
Henri Heyden <henri@heyden5.de>
2025-10-18 16:05:25 +0200

    Implemented review fixes

--- yaau.sh
+++ yaau.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # Check the number of arguments, or if one is "--help", or "-h".
 if [ "$#" -lt 1 ] || [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
@@ -26,10 +27,10 @@ fi
 
 # Check if $1 is really a fitting path.
 DIR="${!#}"
-if [ ! -d $DIR ]; then
+if [ ! -d "$DIR" ]; then
     printf "$DIR is not a path to a directory.\n"
-    # Recursively call yauu.sh --help
-    sh -c "$0 --help"
+    # Recursively call yaau.sh --help
+    "$0 --help"
     exit
 fi
 
@@ -44,11 +45,11 @@ if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
 
     for SUB_DIR in $DIR/*; do
         # Check if SUB_DIR is really a directory of a repository.
-        if [ ! -d $SUB_DIR/.git ]; then
+        if [ ! -d "$SUB_DIR/.git" ]; then
             continue
         fi
         
-        makepkg --needed -sirc -D $SUB_DIR
+        makepkg --needed -sirc -D "$SUB_DIR"
         printf "Installed package from $SUB_DIR.\n"
     done
     
@@ -58,30 +59,30 @@ fi
 # DEFAULT BEHAVIOUR
 printf "Checking all repos in $DIR for updates.\n"
 UPDATE_COUNT=0
-REPO_LIST=""
+REPO_ARRAY=()
 
 # Go through all indexes in the directory, and do the magic...
-for SUB_DIR in $DIR/*; do
+for SUB_DIR in "$DIR"/*; do
     # Check if SUB_DIR is really a directory of a repository.
-    if [ ! -d $SUB_DIR/.git ]; then
+    if [ ! -d "$SUB_DIR/.git" ]; then
         continue
     fi
 
     # Fetch the repos to refresh origin/master.
     # Redirect stdout to /dev/null.
-    git -C $SUB_DIR fetch 1> /dev/null
+    git -C "$SUB_DIR" fetch 1> /dev/null
 
     # Check if master and origin/master differ.
-    OUTPUT=$(git -C $SUB_DIR status -sb)
-    case $OUTPUT in
+    OUTPUT=$(git -C "$SUB_DIR" status -sb)
+    case "$OUTPUT" in
         *"behind"*)
             printf "Repository \"$SUB_DIR\" can be updated.\n"
-            UPDATE_COUNT=$(($UPDATE_COUNT + 1))
-            REPO_LIST="$REPO_LIST,$SUB_DIR"
+            UPDATE_COUNT="$(($UPDATE_COUNT + 1))"
+            REPO_ARRAY+=("$SUB_DIR")
     esac
 done
 
-if [ $UPDATE_COUNT -eq 0 ]; then
+if [ "$UPDATE_COUNT" -eq 0 ]; then
     printf "There are currently no possible updates.\n"
     exit
 fi
@@ -95,18 +96,9 @@ fi
 
 printf "Updating...\n"
 
-# Remove the first element of the list, as it is empty.
-REPO_LIST=$(printf "$REPO_LIST" | cut -c 2-)
-
-# Setup for iterating though the list
-OLD_IFS=$IFS
-IFS=","
-set -- $REPO_LIST # Sets $0, $1, etc to the relevant elements.
-IFS=$OLD_IFS
-
 # Go through all 
-for SUB_DIR; do
-    git -C $SUB_DIR pull 1> /dev/null
-    makepkg -sirc -D $SUB_DIR
-    printf "Updated package from $SUB_DIR\n".
+for SUB_DIR in "${REPO_ARRAY[@]}"; do
+    git -C "$SUB_DIR" pull 1> /dev/null
+    makepkg --needed -sirc -D "$SUB_DIR"
+    printf "Updated package from $SUB_DIR.\n"
 done
